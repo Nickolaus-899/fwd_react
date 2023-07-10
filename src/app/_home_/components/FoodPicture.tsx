0aa1b74aa2a078d3a70999ca318062e793f5c3a1
unknown <n.petukhov@innopolis.university>
2023-07-11 00:37:42 +0300

    react

---
+++ src/app/_home_/components/FoodPicture.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React, {useEffect, useState} from 'react'
+import food from '@/app/_home_/images/food.jpg'
+import Image from "next/image";
+
+function FoodPicture() {
+
+    return (
+        <div className="FoodWrapper">
+            <Image src={food} alt={"food"} className="FoodItem"/>
+        </div>
+    )
+}
+
+export default FoodPicture
--- src/app/_home_/components/HomeMain.tsx
+++ src/app/_home_/components/HomeMain.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import FoodPicture from "@/app/_home_/components/FoodPicture";
+import Image from "next/image";
+import food from "@/app/_home_/images/food.jpg";
 function HomeMain() {
     return (
-        <div>Home</div>
+        <div>
+            <FoodPicture/>
+        </div>
     )
 }
 
