717c57e3627e30429ce7dd36841ed9ff0f9ba6ad
unknown <n.petukhov@innopolis.university>
2023-07-11 20:26:44 +0300

    react

--- src/app/about/components/AboutMain.tsx
+++ src/app/about/components/AboutMain.tsx
@@ -1,58 +1,95 @@
 import React from 'react'
-import Image from "next/image";
+import Image, {StaticImageData} from "next/image";
 
 import cava from '@/app/about/images/cava.jpg'
 import cacio from '@/app/about/images/cacio.jpg'
 import burger from '@/app/about/images/Burger-and-Beer-108.jpg'
 import messa from '@/app/about/images/messa.jpg'
 import kuji from '@/app/about/images/kuji.png'
+import PartnerLogo from "@/app/about/components/PartnerLogo";
+
+export interface PartnerInfo {
+    name: string,
+    url: string,
+    image: string | StaticImageData
+}
 
 function AboutMain() {
-    const PartnersTitles: string[] = [
-        'BigBoss',
-        'Insight',
-        'Patefon',
-        'Innomax',
-        'Jonjoli'
+    const PartnersTitles: PartnerInfo[] = [
+        {
+            name: 'BigBoss',
+            url: 'http://bigbosscafe.tilda.ws/',
+            image: ''
+        },
+        {
+            name: 'Insight',
+            url: 'https://eda.yandex.ru/innopolis/r/insight',
+            image: ''
+        },
+        {
+            name: 'Patefon',
+            url: 'https://eda.yandex.ru/innopolis/r/patefon',
+            image: ''
+        },
+        {
+            name: 'Innomax',
+            url: 'https://foodeon.com/venue/innomax-tivs',
+            image: ''
+        },
+        {
+            name: 'SEasLOc',
+            url: 'https://seasloc.ru/',
+            image: ''
+        }
+    ]
+
+    const firstItem: PartnerInfo = {
+            name: 'cava',
+            url: 'http://www.coffeecava.ru/',
+            image: cava
+    }
+
+    const PartnersLogos: PartnerInfo[] = [
+        {
+            name: 'burger',
+            url: 'https://eda.yandex.ru/innopolis/r/bar_108',
+            image: burger
+        },
+        {
+            name: 'cacio',
+            url: 'https://eda.yandex.ru/innopolis/r/cacio_e_vino',
+            image: cacio
+        },
+        {
+            name: 'messa',
+            url: 'http://messaeres.tilda.ws/',
+            image: messa
+        },
+        {
+            name: 'kuji',
+            url: 'https://kujisushi.ru/',
+            image: kuji
+        }
     ]
     return (
         <div>
             <div className="AboutLogo">Our Partners</div>
             <div className="Partners">
-                <div className="SinglePartner">
-                    <a href="http://www.coffeecava.ru/">
-                        <Image className="SinglePartnerImage PartnerLogo" src={cava} alt={"cava"}/>
-                    </a>
-                </div>
+                <PartnerLogo item={firstItem}/>
                 <div className="SinglePartner">
                     <div className="PartnersList">
                         {
                             PartnersTitles.map((partner, i) => (
-                                <a key={i}>{partner}</a>
+                                <a href={partner.url} key={i}>{partner.name}</a>
                             ))
                         }
                     </div>
                 </div>
-                <div className="SinglePartner">
-                    <a href="https://eda.yandex.ru/innopolis/r/bar_108">
-                        <Image className="SinglePartnerImage PartnerLogo" src={burger} alt={"burger"}/>
-                    </a>
-                </div>
-                <div className="SinglePartner">
-                    <a href="https://eda.yandex.ru/innopolis/r/cacio_e_vino">
-                        <Image className="SinglePartnerImage PartnerLogo" src={cacio} alt={"cacio"}/>
-                    </a>
-                </div>
-                <div className="SinglePartner">
-                    <a href="http://messaeres.tilda.ws/">
-                        <Image className="SinglePartnerImage PartnerLogo" src={messa} alt={"messa"}/>
-                    </a>
-                </div>
-                <div className="SinglePartner">
-                    <a href="https://kujisushi.ru/">
-                        <Image className="SinglePartnerImage PartnerLogo" src={kuji} alt={"innomax"}/>
-                    </a>
-                </div>
+                {
+                    PartnersLogos.map((partner, i) => (
+                        <PartnerLogo item={partner} key={i}/>
+                    ))
+                }
             </div>
         </div>
     )
---
+++ src/app/about/components/PartnerLogo.tsx
@@ -0,0 +1,16 @@
+import React, {JSX} from 'react'
+import Image from "next/image";
+import cava from "@/app/about/images/cava.jpg";
+import {PartnerInfo} from "@/app/about/components/AboutMain";
+
+const PartnerLogo: ({item} : { item: PartnerInfo }) => JSX.Element = ({item}) => {
+    return (
+        <div className="SinglePartner">
+            <a href={item.url}>
+                <Image className="SinglePartnerImage PartnerLogo" src={item.image} alt={item.name}/>
+            </a>
+        </div>
+    )
+}
+
+export default PartnerLogo
