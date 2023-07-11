bcedb53941374b8f4f92e54f2b50af7b875ff828
unknown <n.petukhov@innopolis.university>
2023-07-11 10:21:00 +0300

    react

---
+++ src/app/all/components/AllMain.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllMain() {
+    return (
+        <div>AllMain</div>
+    )
+}
+
+export default AllMain
---
+++ src/app/all/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AllMain from "@/app/all/components/AllMain";
+
+function Page() {
+    return (
+        <div>
+            <AllMain/>
+        </div>
+    )
+}
+
+export default Page
