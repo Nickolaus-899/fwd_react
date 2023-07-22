5d4ffb34767415d2144d0fdf29fc2b532124be52
unknown <n.petukhov@innopolis.university>
2023-07-22 15:42:02 +0300

    authorization and local storage

--- src/app/auth/user/[account]/page.tsx
+++ src/app/auth/user/[account]/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client"
+import React, {useEffect, useState} from 'react'
+import {nullClient, userInfoKey, UserTokenInfo} from "@/app/classes";
+import {fetchData} from "@/app/fetch";
 
 function Page({params} : {params : {account: string}}) {
+    const [client, setClient] = useState(nullClient)
+    const [userName, setUserName] = useState("")
+
+    async function loadClient() {
+        fetchData().then(clients => {
+            for (let i = 0; i < clients.length; i++) {
+                if (clients[i].token === params.account) {
+                    setClient(clients[i])
+                    break
+                }
+            }
+        })
+
+        let check = localStorage.getItem(userInfoKey)
+        if (check) {
+            let token: UserTokenInfo = JSON.parse(localStorage.getItem(userInfoKey) as string)
+            setUserName(token.name)
+        } else {
+            console.log('User is not authorized')
+        }
+    }
+
+    useEffect(() => {
+        loadClient()
+    }, [])
     return (
-        <div>Account of a user {params.account}</div>
+        <div>Account of a user {userName}</div>
     )
 }
 
