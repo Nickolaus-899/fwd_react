320d41c572fa7d0dd4948480c9a5bd992b0505aa
unknown <n.petukhov@innopolis.university>
2023-07-23 17:06:53 +0300

    easter egg

--- src/lib/wrapper/footer/components/SomeText.tsx
+++ src/lib/wrapper/footer/components/SomeText.tsx
@@ -3,7 +3,12 @@ import '@/lib/wrapper/css/position.css'
 
 function SomeText() {
     return (
-        <div className="FooterText SomeText"><div>InnoFood</div><text>N&K</text></div>
+        <div className="FooterText SomeText">
+            <div>InnoFood</div>
+            <text className="Secret1">N</text>
+            <text className="Secret2">&</text>
+            <text className="Secret3">K</text>
+        </div>
     )
 }
 
