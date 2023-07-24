7d490922ccfd175e407ea4d0152fd9499e4e2a22
unknown <n.petukhov@innopolis.university>
2023-07-24 04:15:52 +0300

    font in footer was changed

--- src/lib/wrapper/footer/components/SomeText.tsx
+++ src/lib/wrapper/footer/components/SomeText.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import '@/lib/wrapper/css/position.css'
+import {Dancing_Script} from "@next/font/google"
 
+
+const dancingScript = Dancing_Script({
+    subsets: ['latin'],
+    weight: ['400']
+})
 function SomeText() {
+    const footerText: string = "FooterText"
+    const someText: string = "SomeText"
+
     return (
-        <div className="FooterText SomeText">
-            <div>InnoFood</div>
-            <text className="Secret1">N</text>
-            <text className="Secret2">&</text>
-            <text className="Secret3">K</text>
-        </div>
+        <>
+            <div className={`${footerText} ${someText} ${dancingScript.className}`}>
+                <div>InnoFood</div>
+                <text className="Secret1">N</text>
+                <text className="Secret2">&</text>
+                <text className="Secret3">K</text>
+            </div>
+        </>
+
     )
 }
 
