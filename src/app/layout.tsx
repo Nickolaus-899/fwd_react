bb5f17a212b00de3bab68eea5df9bdf31eeba980
unknown <n.petukhov@innopolis.university>
2023-07-25 02:29:14 +0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,8 @@ const montserrat = Montserrat({
 
 export const metadata = {
   title: "InnoFood",
-  description: "Home Page",
+  description: "Menu for Innopolis",
+
 };
 
 export default function RootLayout({
@@ -23,15 +24,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <>
+      <head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+
+          <meta property="og:url" content="https://fwd-react.vercel.app/"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="InnoFood"/>
+          <meta property="og:description" content="Menu for Innopolis"/>
+          <meta property="og:image" content="https://gagaru.club/uploads/posts/2023-02/1676149591_gagaru-club-p-dieticheskie-interesnie-blyuda-pinterest-88.jpg"/>
         {/*<title>InnoMenu</title>*/}
         {/*<Head>*/}
         {/*  <title>InnoMenu</title>*/}
         {/*</Head>*/}
-      </>
+      </head>
 
       <body className={montserrat.className}>
         <Header />
